48e1bc3d0be94300cabb059987d7515745ca4cfa
Artsiom_Markushevski <lakopatidan@gmail.com>
2025-11-29 18:08:06 +0300

    refactoring(contact): создал отдельную папку с контактами

---
+++ client/src/contact/contacts.jsx
@@ -0,0 +1,8 @@
+﻿const contacts = [
+    { id: 1, name: "Иван Иванов", email: "ivan.ivanov@example.com" },
+    { id: 2, name: "Пётр Петров", email: "petr.petrov@example.com" },
+    { id: 3, name: "Анна Смирнова", email: "anna.smirnova@example.com" },
+    { id: 4, name: "Сергей Кузнецов", email: "sergey.kuznetsov@example.com" },
+];
+
+export default contacts;
