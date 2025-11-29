fa518e4c58ed2def4c9e1f206297d81bad92399f
Artsiom_Markushevski <lakopatidan@gmail.com>
2025-11-29 18:20:57 +0300

    refactor(App): сделали рефакторинг App.js
    - перенесли куски кода в отдельные файлы
    - подключили эти файлы в App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
-﻿import "./App.css";
-
-const contacts = [
-    { id: 1, name: "Иван Иванов", email: "ivan.ivanov@example.com" },
-    { id: 2, name: "Пётр Петров", email: "petr.petrov@example.com" },
-    { id: 3, name: "Анна Смирнова", email: "anna.smirnova@example.com" },
-    { id: 4, name: "Сергей Кузнецов", email: "sergey.kuznetsov@example.com" },
-];
+﻿// src/App.jsx
+import "./App.css";
+import TableContact from "./layout/TableContact/TableContact";
+import contacts from "./contact/contacts";
 
 const App = () => {
     return (
@@ -16,25 +12,7 @@ const App = () => {
                 </div>
 
                 <div className="card-body">
-                    <table className="table table-hover table-bordered mb-0">
-                        <thead className="table-light">
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Имя контакта</th>
-                                <th scope="col">Email</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            {contacts.map((contact, index) => (
-                                <tr key={contact.id}>
-                                    <td>{index + 1}</td>
-                                    <td>{contact.name}</td>
-                                    <td>{contact.email}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                    <TableContact contacts={contacts} />
                 </div>
             </div>
         </div>
@@ -42,4 +20,5 @@ const App = () => {
 };
 
 export default App;
+
 
