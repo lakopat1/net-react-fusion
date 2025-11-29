ec981c0feae2369ad1cc7ae72fc69169c6225902
Artsiom_Markushevski <lakopatidan@gmail.com>
2025-11-29 18:10:40 +0300

    refactoring(TableContact): отдельный файл отрисовки таблицы

---
+++ client/src/layout/TableContact/TableContact.jsx
@@ -0,0 +1,28 @@
+﻿import React from "react";
+
+const TableContact = ({ contacts }) => {
+    return (
+        <table className="table table-hover table-bordered mb-0">
+            <thead className="table-light">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Имя контакта</th>
+                    <th scope="col">Email</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {contacts.map((contact, index) => (
+                    <tr key={contact.id}>
+                        <td>{index + 1}</td>
+                        <td>{contact.name}</td>
+                        <td>{contact.email}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
+export default TableContact;
+
