5c7f52eed265e4c56ca257f292d16e1d49007193
Artsiom_Markushevski <lakopatidan@gmail.com>
2026-01-16 11:06:03 +0300

    feach(Table): создали таблицу контактов

--- client/src/layout/TableContact/TableContact.jsx
+++ client/src/layout/TableContact/TableContact.jsx
@@ -1,28 +1,33 @@
 ﻿import React from "react";
 
 const TableContact = ({ contacts }) => {
-    return (
-        <table className="table table-hover table-bordered mb-0">
-            <thead className="table-light">
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Имя контакта</th>
-                    <th scope="col">Email</th>
-                </tr>
-            </thead>
+  return (
+    <table className="table table-hover table-bordered mb-0">
+      <thead className="table-light">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Имя</th>
+          <th scope="col">Мобильный</th>
+          <th scope="col">Должность</th>
+          <th scope="col">Дата рождения</th>
+        </tr>
+      </thead>
 
-            <tbody>
-                {contacts.map((contact, index) => (
-                    <tr key={contact.id}>
-                        <td>{index + 1}</td>
-                        <td>{contact.name}</td>
-                        <td>{contact.email}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    );
+      <tbody>
+        {contacts.map((contact, index) => (
+          <tr key={contact.id}>
+            <td>{index + 1}</td>
+            <td>{contact.name}</td>
+            <td>{contact.mobilePhone}</td>
+            <td>{contact.jobTitle}</td>
+            <td>{(contact.birthDate)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default TableContact;
+
 
