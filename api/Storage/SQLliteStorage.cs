f176a70fb162abaf772f393b2ea11af04256aca4
Artsiom_Markushevski <lakopatidan@gmail.com>
2026-01-22 18:15:43 +0300

    feach(CRUD): реализовал GET в БД

--- api/Storage/SQLliteStorage.cs
+++ api/Storage/SQLliteStorage.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
 
 public interface IStorage
 {
@@ -8,7 +10,49 @@ public interface IStorage
     bool UpdateContact(ContactDto contactDto, int id);
 }
 
-public class SQLliteStorage : IStorage
+public class SQLiteStorage : IStorage
 {
-   
+    private readonly string connectionString = "Data Source=contacts.db";
+
+    public List<Contact> GetContacts()
+    {
+        var contacts = new List<Contact>();
+
+        using var connection = new SqliteConnection(connectionString);
+        connection.Open();
+
+        using var command = connection.CreateCommand();
+        command.CommandText = "SELECT Id, Name, MobilePhone, JobTitle, BirthDate FROM Contacts;";
+
+        using var reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            contacts.Add(new Contact
+            {
+                Id = reader.GetInt32(0),
+                Name = reader.GetString(1),
+                MobilePhone = reader.GetString(2),
+                JobTitle = reader.GetString(3),
+                BirthDate = DateTime.Parse(reader.GetString(4))
+            });
+        }
+
+        return contacts;
+    }
+
+
+    public bool Add(Contact contact)
+    {
+
+    }
+
+    public bool Remove(int id)
+    {
+
+    }
+
+    public bool UpdateContact(ContactDto contactDto, int id)
+    {
+
+    }
 }
