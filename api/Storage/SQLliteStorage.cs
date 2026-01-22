2d1c056bc744884c0eaeccadae12324247e94f55
Artsiom_Markushevski <lakopatidan@gmail.com>
2026-01-22 17:04:31 +0300

    feach(SQL): создали класс SQL storage  и релаизовлаи интерфейс

---
+++ api/Storage/SQLliteStorage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public interface IStorage
+{
+    List<Contact> GetContacts();
+    bool Add(Contact contact);
+    bool Remove(int id);
+    bool UpdateContact(ContactDto contactDto, int id);
+}
+
+public class SQLliteStorage : IStorage
+{
+    public List<Contact> GetContacts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Add(Contact contact)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool UpdateContact(ContactDto contactDto, int id)
+    {
+        throw new NotImplementedException();
+    }
+
+
+}
